4ecc4311d7bb92ab97458916349058393d87e0a9
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-07-13 10:12:48 +0200

    trying it

---
+++ hello/AccessControl.java
@@ -0,0 +1,19 @@
+package hello;
+
+public class AccesControl {
+
+  public static void main(String[] args) {
+
+  }
+
+}
+
+Class Exo {
+
+  static String text = "accessing from another class";
+
+  static void printText() {
+    System.out.println(text);
+  }
+
+}
