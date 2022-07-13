f0da0c6c30fc66bf56c6d71d77c9aea110333e2a
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-07-13 10:26:03 +0200

    learning loop in java

---
+++ hello/Looping.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class Looping {
+
+  public static void main(String[] args) {
+
+    bonjour("Jean");
+
+  }
+
+  static void bonjour(String nom) {
+    System.out.println("Bonjour " + nom + " !");
+  }
+
+}
