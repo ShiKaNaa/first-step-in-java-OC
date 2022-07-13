d216756324f6d5af25d998dd8f943c9da37ab295
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-07-13 10:21:04 +0200

    finishde for this morning

--- hello/AccesControl.java
+++ hello/AccesControl.java
@@ -3,7 +3,7 @@ package hello;
 public class AccesControl {
 
   public static void main(String[] args) {
-    System.out.println(Exo.text);
+    Exo.printText();
   }
 
 }
@@ -12,8 +12,13 @@ class Exo {
 
   static String text = "accessing from another class";
 
+  // si on met private devant static String... il y aura une erreur car la var text est uniquement disponible dans cette class
+  // si on met public la variable est disponible pour d'autres class
+
   static void printText() {
     System.out.println(text);
   }
+
+  // on peut mettre ici aussi private pour interdir l'acces à la fonction par les autres class
 
 }
