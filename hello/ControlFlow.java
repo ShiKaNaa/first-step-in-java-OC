b41233151946d448889055f24050ca1dfeac96c5
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-07-18 08:45:00 +0200

    if working

--- hello/ControlFlow.java
+++ hello/ControlFlow.java
@@ -4,10 +4,20 @@ public class ControlFlow {
 
   public static void main(String[] args) {
 
+    if(getBoolean()) {
+      print("if");
+    } else {
+      print("else");
+    }
+
   }
 
   static void print(String text) {
     System.out.println("Condition : " + text);
+  }
+
+  static boolean getBoolean() {
+    return true;
   }
 
 }
