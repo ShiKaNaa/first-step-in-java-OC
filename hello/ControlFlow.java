49a256e989dc4a1b4d0a958a28221a3d28ef4c09
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-07-18 08:42:54 +0200

    control flow

---
+++ hello/ControlFlow.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class ControlFlow {
+
+  public static void main(String[] args) {
+
+  }
+
+  static void print(String text) {
+    System.out.println("Condition : " + text);
+  }
+
+}
